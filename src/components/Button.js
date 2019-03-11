5e15886eded89f696a0d6c8a38ad4694a835e6fd
o2sh <ossama-hjaji@live.fr>
2019-03-11 01:45:08 +0100

    navabr continue

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,17 @@
 import styled from 'styled-components';
 
 export const ButtonContainer = styled.button`
+color:white
 text-transform:capitalize;
-font-size:1.4rem;
-background: transparent;
-border:0.O5rem solid blue;
-border-radius: 0.5rem;
+font-size:1rem;
+background-color: #00a7ec;
+border-radius: 0.2rem;
+padding: 0.2rem 0.5rem
 cursor:pointer;
-margin:0.2rem 0.5rem 0.2rem 0;
 transition:all 0.5s ease-in-out;
+border:0;
 &:hover{
-    background:green;
-    color:red;
+    background:#0086bd;
 }
 &:focus{
     outline: none;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,22 +6,19 @@ import { ButtonContainer } from './Button'
 export default class Navbar extends Component {
     render() {
         return (
-            <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
+            <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-3">
                 <Link to='/' className="navbar-brand">
                     codl
                 </Link>
-                <ul className="navbar-nav mr-auto">
-                    <li className="nav-item active">About</li>
-                </ul>
                 <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    <input class="form-control bg-dark mr-sm-2 border-0" type="search" placeholder="Search" aria-label="Search"/>
                 </form>
+                <ul className="navbar-nav bd-navbar-nav flex-row">
+                    <li className="nav-item">About</li>
+                    <li className="nav-item">About</li>
+                </ul>
                 <Link to='/' className="ml-auto">
                     <ButtonContainer>
-                        <span className="mr-2">
-                            <i class="fa fa-sign-in" aria-hidden="true" />
-                        </span>
                         Sign in
                     </ButtonContainer>
                 </Link>
@@ -33,6 +30,6 @@ export default class Navbar extends Component {
 const NavWrapper = styled.nav`
         background:#24292e;
         color:white;
-        font-size:1.3rem;
+        font-size:1rem;
         text-transform:capitalize;
 `
