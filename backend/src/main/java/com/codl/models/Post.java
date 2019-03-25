f570ea03f2faef53812a4cb11340991409a7dee2
o2sh <ossama-hjaji@live.fr>
2019-03-25 08:41:27 +0100

    ...

--- backend/src/main/java/com/codl/models/Post.java
+++ backend/src/main/java/com/codl/models/Post.java
@@ -2,9 +2,14 @@ package com.codl.models;
 
 public class Post {
 	
-	public String title;
-	public String code;
-	public String description;
+	public String Title;
+	public String Code;
+	public String Description;
 
+	public Post(String title, String code, String description) {
+		this.Title = title;
+		this.Code = code;
+		this.Description = description;
+	}
 
 }
--- backend/src/main/java/com/codl/rest/WebServices.java
+++ backend/src/main/java/com/codl/rest/WebServices.java
@@ -1,23 +1,38 @@
 package com.codl.rest;
 
+import java.io.IOException;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import com.codl.models.Post;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-@Path("/")
+@Path("/rest")
 public class WebServices {
-	
+
 	@GET
-	@Path("/Posts")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Post getPosts() {
-		
-		Post post = null;
-		return post;
-		
-	}
+	public String getPosts() {
+		String response="";
+
+		Post post = new Post("Title", "<html>Hello World</html>", "This si the first code");
+		ObjectMapper Obj = new ObjectMapper();
+
+		try {
+
+			 response = Obj.writeValueAsString(post);
+
+		}
+
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	return response;
+
+}
 
 }
