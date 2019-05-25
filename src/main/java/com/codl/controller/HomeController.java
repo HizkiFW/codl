b1b88f3c9558d1d2aaa52907af03f08f4275966a
o2sh <ossama-hjaji@live.fr>
2019-05-25 11:13:15 +0000

    handle all paths with spring controller

--- src/main/java/com/codl/controller/HomeController.java
+++ src/main/java/com/codl/controller/HomeController.java
@@ -1,14 +1,16 @@
 package com.codl.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
-	
-	@GetMapping("/")
+
+	@RequestMapping(method = { RequestMethod.OPTIONS, RequestMethod.GET }, path = { "/post/**", "/t/**", "/submit/**",
+			"/" })
 	public String index() {
-		return "index.html";
+		return "forward:/index.html";
 	}
 
 }
